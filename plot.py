2d4e875083bd89261645d12bd6fdb00f302f4512
Darren Andrean Djaya <129699791+djayadarren@users.noreply.github.com>
2024-02-13 11:26:08 -0800

    plot.py

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ import sys, time, math
 import serial
 
 ser = serial.Serial(
-port='/dev/cu.usbserial-D30HLDBJ',
+port='', # Port COM
 baudrate=115200,
 parity=serial.PARITY_NONE,
 stopbits=serial.STOPBITS_TWO,
@@ -15,38 +15,56 @@ bytesize=serial.EIGHTBITS
 ser.isOpen()
 
 xsize=100
-   
+
 def data_gen():
     t = data_gen.t
     while True:
        t+=1
-       #val=abs(250.0*math.sin(t*2.0*3.1415/100.0))
-       #val=t
-       strin = ser.readline()
-       strin = strin.rstrip()
-       strin = strin.decode()
-       val=float(strin)
-       yield t, val
+       strin = ser.readline()       # Read from port
+       parts = strin.split('.')     # Split temp.state
+       val = int(parts[0])          # temp to val
+       state = int(parts[1])        # state to state
+       yield t, val, state
 
 def run(data):
     # update the data
-    t,y = data
+    t,y,state = data
     if t>-1:
         xdata.append(t)
         ydata.append(y)
+        statedata.append(state)
         if t>(xsize-50): # Scroll to the left.
             ax.set_xlim(t-xsize+50, t+50)
 
         if y>50: # Scroll to the left.
             ax.set_ylim(y-50, y+50)
-            ax.set
         
+        # Gradient stuff
         norm_y = round((220.1-(y)))
         color = mcolors.to_rgba(plt.cm.RdBu(norm_y))
+        ax.plot(xdata[-3:], ydata[-3:], color=color, linewidth=4)
+        ax.set_facecolor(mcolors.to_rgba(plt.cm.RdBu(norm_y+60)))
+        
+        if(state == 0): # chillin state
+            ax.set_title('BING CHILLING', fontdict={'fontsize': 14, 'fontweight': 'bold','fontfamily':'Times New Roman'})
 
-        ax.plot(xdata[-3:], ydata[-3:], color=color, linewidth=5)
-        # line.set_data(xdata, ydata)
+        if(state == 1): # ramp
+            ax.set_title('Ramp')
 
+        if(state == 2): # soak
+            ax.set_title('Soak')
+
+        if(state == 3): # peak
+            ax.set_title('Peak')
+
+        if(state == 4): # reflow
+            ax.set_title('Reflow')
+
+        if(state == 5): # coolin
+            ax.set_title('Cooling')
+
+        line.set_data(xdata, ydata)
+            
     return line,
 
 def on_close_figure(event):
@@ -56,16 +74,15 @@ data_gen.t = -1
 fig = plt.figure()
 fig.canvas.mpl_connect('close_event', on_close_figure)
 ax = fig.add_subplot(111)
-line, = ax.plot([], [], lw=5)
+line, = ax.plot([], [], lw=4)
 line.set_color(mcolors.to_rgba(plt.cm.RdBu(230)))
 ax.set_ylim(0, 100)
 ax.set_xlim(0, xsize)
 ax.grid()
-xdata, ydata = [], []
-colors = []
+xdata, ydata, statedata = [], [], []
 ax.set_xlabel('Time',font='Helvetica')
 ax.set_ylabel('Temp',font='Helvetica')
-
+ax.set_facecolor('darkgray')
 
 # Important: Although blit=True makes graphing faster, we need blit=False to prevent
 # spurious lines to appear when resizing the stripchart.
