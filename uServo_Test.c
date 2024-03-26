5d14b9832b85dc5af357cb5c80c3f9b248457df7
GMSadly <159588745+GMSadly@users.noreply.github.com>
2024-03-26 10:35:27 -0700

    Motor Driver base code
    Haven't tested if it works yet, but this should have a good starting point for the PWM cycles. We need to change the 50 and 75 to separate variables then wire the OUT pins as well as create the code logic.

---
+++ uServo_Test.c
@@ -0,0 +1,180 @@
+//  EFM8_Servo.c: Uses timer 5 interrupt to generate a servo motor control signal.
+//
+//  Copyright (c) 2010-2018 Jesus Calvino-Fraga
+//  ~C51~
+
+#include <EFM8LB1.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+//volatile unsigned int servo_counter=0;
+//volatile unsigned char servo1=150, servo2=150;
+
+#define OUT0 P2_0 //MOTOR pin
+#define OUT1 P2_1 //MOTOR pin
+#define DEFAULT_F 15500L
+
+#define COLPITTS P1_5
+#define EMAGNET  P1_6
+
+#define SYSCLK 72000000L // SYSCLK frequency in Hz
+#define BAUDRATE 115200L
+#define RELOAD_10us (0x10000L-(SYSCLK/(12L*100000L))) // 10us rate
+
+volatile unsigned char pwm_count=0;
+
+char _c51_external_startup (void)
+{
+	// Disable Watchdog with key sequence
+	SFRPAGE = 0x00;
+	WDTCN = 0xDE; //First key
+	WDTCN = 0xAD; //Second key
+  
+	VDM0CN |= 0x80;
+	RSTSRC = 0x02;
+
+	#if (SYSCLK == 48000000L)	
+		SFRPAGE = 0x10;
+		PFE0CN  = 0x10; // SYSCLK < 50 MHz.
+		SFRPAGE = 0x00;
+	#elif (SYSCLK == 72000000L)
+		SFRPAGE = 0x10;
+		PFE0CN  = 0x20; // SYSCLK < 75 MHz.
+		SFRPAGE = 0x00;
+	#endif
+	
+	#if (SYSCLK == 12250000L)
+		CLKSEL = 0x10;
+		CLKSEL = 0x10;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 24500000L)
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 48000000L)	
+		// Before setting clock to 48 MHz, must transition to 24.5 MHz first
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+		CLKSEL = 0x07;
+		CLKSEL = 0x07;
+		while ((CLKSEL & 0x80) == 0);
+	#elif (SYSCLK == 72000000L)
+		// Before setting clock to 72 MHz, must transition to 24.5 MHz first
+		CLKSEL = 0x00;
+		CLKSEL = 0x00;
+		while ((CLKSEL & 0x80) == 0);
+		CLKSEL = 0x03;
+		CLKSEL = 0x03;
+		while ((CLKSEL & 0x80) == 0);
+	#else
+		#error SYSCLK must be either 12250000L, 24500000L, 48000000L, or 72000000L
+	#endif
+	
+	// Configure the pins used for square output
+	P2MDOUT|=0b_0000_0011;
+	P0MDOUT |= 0x10; // Enable UART0 TX as push-pull output
+	XBR0     = 0x01; // Enable UART0 on P0.4(TX) and P0.5(RX)                     
+	XBR1     = 0X10; // Enable T0 on P0.0
+	XBR2     = 0x40; // Enable crossbar and weak pull-ups
+
+	#if (((SYSCLK/BAUDRATE)/(2L*12L))>0xFFL)
+		#error Timer 0 reload value is incorrect because (SYSCLK/BAUDRATE)/(2L*12L) > 0xFF
+	#endif
+	// Configure Uart 0
+	SCON0 = 0x10;
+	CKCON0 |= 0b_0000_0000 ; // Timer 1 uses the system clock divided by 12.
+	TH1 = 0x100-((SYSCLK/BAUDRATE)/(2L*12L));
+	TL1 = TH1;      // Init Timer1
+	TMOD &= ~0xf0;  // TMOD: timer 1 in 8-bit auto-reload
+	TMOD |=  0x20;                       
+	TR1 = 1; // START Timer1
+	TI = 1;  // Indicate TX0 ready
+
+	// Initialize timer 2 for periodic interrupts
+	TMR2CN0=0x00;   // Stop Timer2; Clear TF2;
+	CKCON0|=0b_0001_0000;
+	TMR2RL=(-(SYSCLK/(2*DEFAULT_F))); // Initialize reload value
+	TMR2=0xffff;   // Set to reload immediately
+	ET2=1;         // Enable Timer2 interrupts
+	TR2=1;         // Start Timer2
+
+	// Initialize timer 5 for periodic interrupts
+	SFRPAGE=0x10;
+	TMR5CN0=0x00;
+	TMR5=0xffff;   // Set to reload immediately
+	EIE2|=0b_0000_1000; // Enable Timer5 interrupts
+	TR5=1;         // Start Timer5 (TMR5CN0 is bit addressable)
+	
+	EA=1;
+	
+	SFRPAGE=0x00;
+	
+	return 0;
+}
+
+void Timer2_ISR (void) interrupt INTERRUPT_TIMER2
+{
+	TF2H = 0; // Clear Timer2 interrupt flag
+	
+	pwm_count++;
+	if(pwm_count>100) pwm_count=0;
+	
+	OUT0=pwm_count>50?0:1;
+	OUT1=pwm_count>75?0:1;
+}
+
+*/
+// Uses Timer3 to delay <us> micro-seconds. 
+void Timer3us(unsigned char us)
+{
+	unsigned char i;               // usec counter
+	
+	// The input for Timer 3 is selected as SYSCLK by setting T3ML (bit 6) of CKCON0:
+	CKCON0|=0b_0100_0000;
+	
+	TMR3RL = (-(SYSCLK)/1000000L); // Set Timer3 to overflow in 1us.
+	TMR3 = TMR3RL;                 // Initialize Timer3 for first overflow
+	
+	TMR3CN0 = 0x04;                 // Sart Timer3 and clear overflow flag
+	for (i = 0; i < us; i++)       // Count <us> overflows
+	{
+		while (!(TMR3CN0 & 0x80));  // Wait for overflow
+		TMR3CN0 &= ~(0x80);         // Clear overflow indicator
+	}
+	TMR3CN0 = 0 ;                   // Stop Timer3 and clear overflow flag
+}
+
+void waitms (unsigned int ms)
+{
+	unsigned int j;
+	for(j=ms; j!=0; j--)
+	{
+		Timer3us(249);
+		Timer3us(249);
+		Timer3us(249);
+		Timer3us(250);
+	}
+}
+//Drive motors from x and y coordinate inputs from serial monitor. 
+//Example (assuming positive x right in direction and y is up)
+// driveMotors(1000, 1000), would drive the car up and turn it
+/*
+void driveMotors (unsigned int x, unsigned int y)
+{
+	if(y > 2.5){
+		//This might be in the incorrect order, 
+		OUT0 = 0;
+	}
+}
+*/
+void main (void)
+{
+	printf("\x1b[2J"); // Clear screen using ANSI escape sequence.
+	printf("Square wave generator for the F38x.\r\n"
+	       "Check pins P2.0 and P2.1 with the oscilloscope.\r\n");
+
+	while(1)
+	{
+	}
+}
